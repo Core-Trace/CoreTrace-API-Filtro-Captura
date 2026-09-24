a33f9b5ed4e5b17188c351eb355bcc1b46af8c2a
raphael-canuto <raphaelcanuto464@gmail.com>
2026-09-24 12:04:16 -0300

    Feat: Metricas corretas na query de buscarMetricasPorToken

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -2,35 +2,24 @@ var database = require('../database/config');
 
 function buscarMetricasPorToken(tokenNode) {
     var instrucaoSql = `
-        SELECT DISTINCT
-            comp.nome,
-            comp.nome_coluna,
-            comp.funcao_psutil,
-            comp.argumento_nome,
-            comp.argumento_valor,
-            comp.atributo_retorno,
-            comp.indice_retorno,
-            comp.unidade
-        FROM node n
-        JOIN cluster c
-            ON c.id = n.fk_cluster
-        JOIN ambiente_hpc a
-            ON a.id = c.fk_ambiente_hpc
-        JOIN empresa e
-            ON e.id = a.fk_empresa
+    SELECT DISTINCT
+        comp.nome,
+        comp.nome_coluna,
+        comp.funcao_psutil,
+        comp.argumento_nome,
+        comp.argumento_valor,
+        comp.atributo_retorno,
+        comp.indice_retorno,
+        comp.unidade
+    FROM servidor s
+    JOIN empresa e
+        ON e.id = s.fk_empresa
+    JOIN componente_servidor cs
+        ON cs.fk_servidor = s.id
+    JOIN componente comp
+        ON comp.id = cs.fk_componente
 
-        JOIN ambiente_hpc a2
-            ON a2.fk_empresa = e.id
-        JOIN cluster c2
-            ON c2.fk_ambiente_hpc = a2.id
-        JOIN node n2
-            ON n2.fk_cluster = c2.id
-        JOIN componente_node cn
-            ON cn.fk_node = n2.id
-        JOIN componente comp
-            ON comp.id = cn.fk_componente
-
-        WHERE n.token_node = '${tokenNode}';
+        WHERE s.token_servidor = '${tokenNode}';
     `;
 
     return database.executar(instrucaoSql);
@@ -38,11 +27,10 @@ function buscarMetricasPorToken(tokenNode) {
 
 function listarLimites(idEmpresa) {
     var instrucaoSql = `
-    SELECT c.nome AS componente, c.unidade, cn.limite_atencao, cn.limite_critico FROM empresa e 
-	JOIN ambiente_hpc a ON a.fk_empresa = e.id
-    JOIN cluster cl ON cl.fk_ambiente_hpc = a.id JOIN node n ON n.fk_cluster = cl.id
-    JOIN componente_node cn ON cn.fk_node = n.id 
-    JOIN componente c ON c.id = cn.fk_componente WHERE e.id = ${idEmpresa};`
+    SELECT c.nome AS componente, c.unidade, cs.limite_atencao, cs.limite_critico FROM empresa e
+    JOIN servidor s ON s.fk_empresa = e.id
+    JOIN componente_servidor cs ON cs.fk_servidor = s.id
+    JOIN componente c ON c.id = cs.fk_componente WHERE e.id = ${idEmpresa};`
 
     return database.executar(instrucaoSql);
 }
